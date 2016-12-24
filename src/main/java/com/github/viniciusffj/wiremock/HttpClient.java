cc33b0a4161919c6862ea720843af9a2839b8cb2
Vinicius Fernandes <viniciusffj@gmail.com>
2016-12-24 13:40:19 -0500

    Add HttpClient

---
+++ src/main/java/com/github/viniciusffj/wiremock/HttpClient.java
@@ -0,0 +1,10 @@
+package com.github.viniciusffj.wiremock;
+
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+public class HttpClient {
+    public void get(String url) throws UnirestException {
+        Unirest.get(url).asJson();
+    }
+}
