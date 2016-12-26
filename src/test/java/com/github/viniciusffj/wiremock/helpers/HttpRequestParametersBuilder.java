585a7be98fdaa95e568b9ac41ea423ccc9137c79
Vinicius Fernandes <viniciusffj@gmail.com>
2016-12-25 23:58:41 -0500

    Add HttpRequestParametersBuilder

---
+++ src/test/java/com/github/viniciusffj/wiremock/helpers/HttpRequestParametersBuilder.java
@@ -0,0 +1,28 @@
+package com.github.viniciusffj.wiremock.helpers;
+
+import com.github.viniciusffj.wiremock.http.HttpMethod;
+import com.github.viniciusffj.wiremock.http.HttpRequestParameters;
+
+public class HttpRequestParametersBuilder {
+
+    private ParametersBuilder parametersBuilder = new ParametersBuilder();
+
+    public HttpRequestParametersBuilder url(String url) {
+        this.parametersBuilder.url(url);
+        return this;
+    }
+
+    public HttpRequestParametersBuilder method(HttpMethod method) {
+        this.parametersBuilder.method(method);
+        return this;
+    }
+
+    public HttpRequestParametersBuilder header(String key, String value) {
+        this.parametersBuilder.header(key, value);
+        return this;
+    }
+
+    public HttpRequestParameters build() {
+        return new HttpRequestParameters(this.parametersBuilder.build());
+    }
+}
